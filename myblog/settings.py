61098b4ffea743b915e828a6217186e1d4de73d0
Aditya-Pala-code <pala338710@gmail.com>
2026-03-15 14:50:55 +0530

    auto create superuser

--- myblog/settings.py
+++ myblog/settings.py
@@ -127,3 +127,16 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if os.environ.get("CREATE_SUPERUSER") == "True":
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if username and password and not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
